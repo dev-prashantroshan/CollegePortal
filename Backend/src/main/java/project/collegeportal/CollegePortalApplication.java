661f1ef1343ce74a98fdfead966fedfe68c12bad
dev-prashantroshan <dev.prashantroshan@gmail.com>
2026-02-11 15:51:42 +0530

    First commit

---
+++ Backend/src/main/java/project/collegeportal/CollegePortalApplication.java
@@ -0,0 +1,13 @@
+package project.collegeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegePortalApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/project/collegeportal/CollegePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package project.collegeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
